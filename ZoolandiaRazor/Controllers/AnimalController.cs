b65b256a1cf412460f224d9f14f1ca14043d6e2c
Ryan B <admirallasers@gmail.com>
2016-10-13 18:31:34 -0500

    Animals page works!

--- ZoolandiaRazor/Controllers/AnimalController.cs
+++ ZoolandiaRazor/Controllers/AnimalController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoolandiaRazor.DAL;
 
 namespace ZoolandiaRazor.Controllers
 {
     public class AnimalController : Controller
     {
+        ZooRepository repo = new ZooRepository();
         // GET: Animal
         public ActionResult Index()
         {
+            ViewBag.animals = repo.GetAnimals();
             return View();
         }
 
         // GET: Animal/Details/5
         public ActionResult Details(int id)
         {
+            ViewBag.Animal = repo.FindAnimal(id);
             return View();
         }
 
